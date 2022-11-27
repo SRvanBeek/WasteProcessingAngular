66d7d71f7a23bcaf0a1af7b3f458c0d3b6948898
Dino Yang <s1136300@student.hsleiden.nl>
2022-11-27 16:40:57 +0100

    feat: add offcanvas menu when window smaller than lg feat: add styles.scss style fix: fixed responsive bugs

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,43 @@ import {Component} from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  screenLGSize: number = 992;
+  typeHistory: string;
+  typeAdmin: string;
 
+
+  ngOnInit() {
+    if (window.innerWidth >= this.screenLGSize) {
+      this.typeHistory = 'history';
+      this.typeAdmin = 'admin';
+    } else {
+      this.typeHistory = 'historyOff';
+      this.typeAdmin = 'adminOff';
+    }
+  }
+
+  @HostListener("window:resize", []) updateHistory() {
+    if (window.innerWidth >= this.screenLGSize) {
+      this.typeHistory = 'history';
+      this.typeAdmin = 'admin';
+    } else {
+      this.typeHistory = 'historyOff';
+      this.typeAdmin = 'adminOff';
+    }
+  }
+
+  @HostListener("document:click", ['$event.target']) clickNavbar(clickPosition: any) {
+    let shownEl = document.getElementsByClassName('show');
+    if (shownEl.length != 0) {
+      let buttonID = shownEl[0].id + 'Button';
+      let subMenu = document.getElementById(shownEl[0].id);
+      let button = document.getElementById(buttonID);
+      if (button) {
+        if (!clickPosition.closest('#navBar') && !clickPosition.closest('#' + subMenu)) {
+          button.dispatchEvent(new CustomEvent('click'));
+        }
+      }
+    }
+  }
 }
