0978b9c13d4a3457c8e41f3e5585d76a5ba2d31c
SRvanBeek <0705stijnvb@gmail.com>
2022-11-27 16:43:10 +0100

    feat: added routing after succesful login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {OrdersComponent} from "./orders/orders.component";
+import {LoginComponent} from "./login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:  LoginComponent},
+  { path: 'orders', component:  OrdersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -60,7 +60,7 @@ export class LoginComponent {
       .subscribe({
         next: () => {
           // get return url from route parameters or default to '/'
-          const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+          const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/orders';
           this.router.navigate([returnUrl]);
         },
         error: error => {
