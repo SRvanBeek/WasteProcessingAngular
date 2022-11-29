36e3a9561faa4988debf89c95e728a58195e6a32
Dino Yang <s1136300@student.hsleiden.nl>
2022-11-29 12:52:11 +0100

    fix-shared module created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,18 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {HeaderComponent} from './header/header.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {LoginModule} from "./login/login.module";
-import {JwtInterceptor} from "./_helper/jwt.interceptor";
+import {JwtInterceptor} from "./shared/_helper/jwt.interceptor";
 import {AdminPanelComponent} from './admin-panel/admin-panel.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {WasteProcessingComponent} from "./waste-processing/waste-processing.component";
+import {SharedModule} from "./shared/shared.module";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     AdminPanelComponent,
     WasteProcessingComponent,
   ],
@@ -22,7 +21,8 @@ import {WasteProcessingComponent} from "./waste-processing/waste-processing.comp
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    LoginModule
+    LoginModule,
+    SharedModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
