fb159991161bca3ea018a5dadac9a0801c3d4b5d
TOGX <s1136032@student.hsleiden.nl>
2022-11-29 12:20:12 +0100

    FEAT: Module added for waste-processing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import {LoginModule} from "./login/login.module";
 import {JwtInterceptor} from "./_helper/jwt.interceptor";
 import {AdminPanelComponent} from './admin-panel/admin-panel.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {WasteProcessingComponent} from "./waste-processing/waste-processing.component";
+import {WasteProcessingModule} from "./waste-processing/waste-processing.module";
 
 
 @NgModule({
@@ -16,13 +16,13 @@ import {WasteProcessingComponent} from "./waste-processing/waste-processing.comp
     AppComponent,
     HeaderComponent,
     AdminPanelComponent,
-    WasteProcessingComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    LoginModule
+    LoginModule,
+    WasteProcessingModule,
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
