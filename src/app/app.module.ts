eb369d9ca8c71a7834e00cec03f94f86acee0d8d
Dino Yang <s1136300@student.hsleiden.nl>
2022-11-29 12:05:41 +0100

    fix- routing fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {LoginModule} from "./login/login.module";
 import {JwtInterceptor} from "./_helper/jwt.interceptor";
-import { OrderListComponent } from './orders/order-list/order-list.component';
-import { OrdersComponent } from './orders/orders.component';
-import { OrderComponent } from './orders/order-list/order/order.component';
-import { OrderDetailsComponent } from './orders/order-list/order-details/order-details.component';
-import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import {AdminPanelComponent} from './admin-panel/admin-panel.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {WasteProcessingComponent} from "./waste-processing/waste-processing.component";
 
@@ -29,9 +25,10 @@ import {WasteProcessingComponent} from "./waste-processing/waste-processing.comp
     LoginModule
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
     BrowserAnimationsModule,
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
