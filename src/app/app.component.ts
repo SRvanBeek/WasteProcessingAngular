967b38405e9fda7070084236e6cbfb50ffccdd55
Dino Yang <s1136300@student.hsleiden.nl>
2022-12-22 00:12:17 +0100

    feat: add sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 
 @Component({
@@ -6,10 +6,20 @@ import {Router} from "@angular/router";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'WasteProcessing-Angular';
+  screenLGSize: number = 992;
+  isDesktop: boolean;
 
 
   constructor(public router: Router) {
+  }
+
+  ngOnInit() {
+    this.updateIsDesktop();
+  }
+
+  @HostListener("window:resize", []) updateIsDesktop() {
+    this.isDesktop = window.innerWidth >= this.screenLGSize;
   }
 }
