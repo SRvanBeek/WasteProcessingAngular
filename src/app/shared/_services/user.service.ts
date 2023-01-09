96d2b72ce3c6c00af32a139b4a24d3994d076580
Dino Yang <s1136300@student.hsleiden.nl>
2023-01-09 13:56:01 +0100

    feat: create user model and service

---
+++ src/app/shared/_services/user.service.ts
@@ -0,0 +1,34 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../../environments/environment";
+import {User} from "../_models/user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getOneUser(username: string): Observable<User> {
+    return this.http.get<User>(environment.apiUrl + '/api/users/' + username);
+  }
+
+  getAllUsers(): Observable<any> {
+    return this.http.get<any>(environment.apiUrl + '/api/users/');
+  }
+
+  checkUsername(username: String): Observable<any> {
+    return this.http.post<any>(environment.apiUrl + '/api/users/checkUsername', username)
+  }
+
+  registerUser(user: User): Observable<any> {
+    return this.http.post<any>(environment.apiUrl + '/api/users/save', user);
+  }
+
+  putUser(user: User): Observable<any> {
+    return this.http.put<any>(environment.apiUrl + '/api/users/', user);
+  }
+}
