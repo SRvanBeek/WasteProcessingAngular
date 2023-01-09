a7447c1e82c9afda74c818ed1415df3a53fc0072
Dino Yang <s1136300@student.hsleiden.nl>
2023-01-09 14:38:40 +0100

    fix: modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,27 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HeaderModule} from "../header/header.module";
-import { DashboardComponent } from './_modals/dashboard/dashboard.component';
+import {DashboardComponent} from './_modals/dashboard/dashboard.component';
 import {ModalModule} from "./_modals/modal.module";
+import {FilterPipe} from './_pipes/filter.pipe';
+import {ToastComponent} from './toast/toast.component';
+import {NgbToastModule} from "@ng-bootstrap/ng-bootstrap";
 
 
 @NgModule({
   declarations: [
+    FilterPipe,
+    ToastComponent
   ],
   imports: [
     CommonModule,
     HeaderModule,
-    ModalModule
+    ModalModule,
+    NgbToastModule
+  ],
+  exports: [
+    FilterPipe,
+    ToastComponent
   ],
   entryComponents: [
     DashboardComponent
