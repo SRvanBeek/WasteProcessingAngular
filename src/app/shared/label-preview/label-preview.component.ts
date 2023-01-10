e21494284091bd71a9c2485ac500a9ae2f074dd8
Roy van Delft <s1121703@student.hsleiden.nl>
2023-01-10 16:09:52 +0100

    FEAT: Added the first models to the Order label

--- src/app/shared/label-preview/label-preview.component.ts
+++ src/app/shared/label-preview/label-preview.component.ts
@@ -1,14 +1,31 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import jsPDF from 'jspdf';
 import {Leftover} from "../_models/leftover.model";
+import {Article} from "../_models/article";
+import {ArticleService} from "../_services/article.service";
+import {LeftoverService} from "../_services/leftover.service";
 
 @Component({
   selector: 'app-label-preview',
   templateUrl: './label-preview.component.html',
   styleUrls: ['./label-preview.component.scss']
 })
-export class LabelPreviewComponent {
+export class LabelPreviewComponent implements OnInit {
   @Input() todo: Leftover;
+
+  article: Article
+  leftover: Leftover
+
+  constructor(private articleService: ArticleService, private leftoverService: LeftoverService)
+   {
+  }
+
+  setArticle(id: string) {
+    this.articleService.getOneArticle(id)
+      .subscribe(value => {
+        this.article = value.payload;
+      });
+  }
 
   @ViewChild('modal', { static: false }) el!:ElementRef;
 
@@ -18,8 +35,14 @@ export class LabelPreviewComponent {
     pdf.html(this.el.nativeElement, {
       callback: (pdf) => {
         pdf.save('OrderLabel'+ '.pdf');
+
       }
   });
 }
+
+  ngOnInit(): void {
+    console.log(this.todo)
+    this.setArticle(this.todo.artikelnummer)
+  }
 }
 
