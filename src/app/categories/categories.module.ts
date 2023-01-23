f94fe4990d00a2f9b987604dba3728918d7f6617
Noah Elstgeest <s1136032@student.hsleiden.nl>
2023-01-23 21:53:46 +0100

    FIX: Fixed ngModule after deleting useless component.

--- src/app/categories/categories.module.ts
+++ src/app/categories/categories.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CategoriesComponent} from "./categories.component";
 import {CategoryInfoBoxComponent} from "./category-info-box/category-info-box.component";
-import { ConditionInputComponent } from './category-info-box/condition-input/condition-input.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { ConditionModalComponent } from './category-info-box/condition-modal/condition-modal.component';
 import {SharedModule} from "../shared/shared.module";
@@ -14,7 +13,6 @@ import {SharedModule} from "../shared/shared.module";
   declarations: [
     CategoriesComponent,
     CategoryInfoBoxComponent,
-    ConditionInputComponent,
     ConditionModalComponent
   ],
   imports: [
